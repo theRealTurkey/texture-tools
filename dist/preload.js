0280dab1e179c8ce9c9ae259fe90093bc0b13574
Turkey Ot <45902287+theRealTurkey@users.noreply.github.com>
2021-10-29 23:40:49 +0300

    delete dist as it can be built

--- dist/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var electron_1 = require("electron");
-var os_1 = require("os");
-electron_1.contextBridge.exposeInMainWorld("api", {
-    threads: (0, os_1.cpus)().length,
-});
